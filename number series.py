ca9673685b757ab8ecab48e0117b465a154ba0da
manoj0807 <60419348+manoj0807@users.noreply.github.com>
2020-01-29 11:46:35 +0530

    Create number series.py

---
+++ number series.py
@@ -0,0 +1,40 @@
+a=int(input("enter the number:"))
+for i in range(0,50):
+    if (i%2==0):
+      print(i)
+      
+...
+samle input:
+enter the number:50
+
+
+
+sample output:
+enter the number:50
+0
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
+32
+34
+36
+38
+40
+42
+44
+46
+48
+'''
+
